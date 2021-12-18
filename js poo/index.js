e6983ec84d948653d13459bb15577154f06254f8
EvertonDSS <74423015+EvertonDSS@users.noreply.github.com>
2021-12-18 16:13:23 -0300

    Add files via upload

---
+++ js poo/index.js
@@ -0,0 +1,25 @@
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
+
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = "11122233309";
+
+const cliente2 = new Cliente();
+cliente2.nome = "Maria";
+cliente2.cpf = "22233344400";
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = "1001";
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
+
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = "102";
+
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
+
+console.log("valor ", valor);
+console.log(conta2);
